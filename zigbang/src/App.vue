95d48b5811e2a4365034c6ac7b50c2cd1817fac5
YeryunJung <2bites@naver.com>
2023-05-02 00:59:56 +0900

    데이터 바인딩

--- zigbang/src/App.vue
+++ zigbang/src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <!-- 바뀔 일이 많이 없으면 하드코딩도 가능(로고 등) -->
+    원룸샵
+    <h4 class="red" :style="스타일">XX 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>XX 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    // 데이터 보관함
+    return {
+      price1: 60,
+      price2: 70,
+      스타일: "color: blue",
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
