ffbbd6e860eef4f933a991479ba1303420d478ea
yeryunJung <2bites@naver.com>
2023-05-08 15:39:57 +0900

    데이터 임포트해와서 컴포넌트에 전달

---
+++ blog/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <h5>React 개발자의 블로그입니다</h5>
+      <p>Vue로 만들었음</p>
+    </div>
+    <!-- <button type="button" class="btn btn-primary">Primary</button> -->
+    <List :블로그글="블로그글" />
+  </div>
+  
+</template>
+
+<script>
+import List from './components/List.vue'
+import blogData from '@/assets/blog.js'
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      블로그글: blogData,
+    }
+  },
+  components: {
+    List,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ blog/src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="(글, idx) in 블로그글" :key="idx">
+      <h5>{{글.title}}</h5>
+      <p>{{글.date}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    블로그글: Array,
+  }
+}
+</script>
+
+<style>
+
+</style>
